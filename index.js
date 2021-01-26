d655d21993687264f34850396b0bf954ed143b39
bluejv7 <bluejv7@gmail.com>
2021-01-25 19:42:31 -0800

    Add deleteMessage(), removeRole(), and setRole()

--- index.js
+++ index.js
@@ -10,10 +10,34 @@ class Discord {
         this.authToken = authToken;
     }
 
+    deleteMessage(channelId, messageId) {
+        return axios.delete(`${BASE_URL}/channels/${channelId}/messages/${messageId}`, {
+            headers: {
+                Authorization: this.authToken
+            }
+        });
+    }
+
+    removeRole(guildId, userId, roleId) {
+        return axios.delete(`${BASE_URL}/guilds/${guildId}/members/${userId}/roles/${roleId}`, {
+            headers: {
+                Authorization: this.authToken
+            }
+        });
+    }
+
     sendMessage(message, channelId) {
         return axios.post(`${BASE_URL}/channels/${channelId}/messages`, {
             content: message
         }, {
+            headers: {
+                Authorization: this.authToken
+            }
+        });
+    }
+
+    setRole(guildId, userId, roleId) {
+        return axios.put(`${BASE_URL}/guilds/${guildId}/members/${userId}/roles/${roleId}`, {}, {
             headers: {
                 Authorization: this.authToken
             }
